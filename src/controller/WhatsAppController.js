40961582a3961f81ebbe901e183e6bc6f44c1053
Guilherme <mttsguil@gmail.com>
2022-08-29 09:08:07 -0300

    add: get panel datas and use FormData

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -88,6 +88,23 @@ class WhatsAppController {
 
         }
 
+        HTMLFormElement.prototype.getForm = function() { 
+            return new FormData(this);
+        }
+
+        HTMLFormElement.prototype.toJSON = function() { 
+
+            let json = {};
+
+            this.getForm().forEach((value, key)=> { 
+
+                json[key] = value;
+            });
+
+            return json;
+        }
+
+
 
     };
 
@@ -123,6 +140,38 @@ class WhatsAppController {
         this.el.btnClosePanelAddContact.on('click', e=> { 
 
             this.el.panelAddContact.removeClass('open');
+
+        });
+
+        this.el.photoContainerEditProfile.on('click', e=> { 
+
+            this.el.inputProfilePhoto.click();
+
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e=> { 
+
+            if(e.key === 'Enter') { 
+
+                e.preventDefault();
+
+                this.el.btnSavePanelEditProfile.click();
+
+            };
+
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e=> { 
+
+            console.log(this.el.inputNamePanelEditProfile.innerHTML)
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e=> { 
+
+            e.preventDefault();
+
+            let formData = new FormData(this.el.formPanelAddContact)
 
         });
 
