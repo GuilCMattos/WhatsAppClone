5b7605464c0fa84e6c989d882a641947eba30db0
Guilherme Mattos <91619478+GuilCMattos@users.noreply.github.com>
2022-08-30 12:53:02 -0300

    Pdf audio and firebase (#2)
    * add: reading PDF archive and image with PDF.js
    
    * add: active microphone with API mediaDevices GetUserMedia
    
    * add: creating class for events
    
    * add: recording audio captured of microphone with MediaRecorder
    
    * add: event to see recording timer
    
    * add: firebase storage and firestore
    
    * add: firebase authetication
    
    * add: saving users authentic in database
    
    * add: application DAO
    
    * add: att name user
    
    * add: add contact in Whatsapp Clone
    
    * add: listing contacts
    
    * add: event click in contact

---
+++ src/utils/Firebase.js
@@ -0,0 +1,76 @@
+const firebase = require('firebase');
+require('firebase/firestore')
+
+export class Firebase { 
+
+    constructor() {
+
+        this._config = { 
+            
+
+                apiKey: "AIzaSyC11t9gUfr-trXSVLe4otU_g9gBNKN2k0s",
+            
+                authDomain: "whatsapp-clone-260ca.firebaseapp.com",
+            
+                projectId: "whatsapp-clone-260ca",
+            
+                storageBucket: "whatsapp-clone-260ca.appspot.com",
+            
+                messagingSenderId: "76403264920",
+            
+                appId: "1:76403264920:web:fe7091e8bf71b14218afed"
+            
+              
+        }
+        this.init();
+    }
+
+    init( ){
+
+        if(!window._initializedFirebase) { 
+            firebase.initializeApp(this._config);
+
+            firebase.firestore().settings({
+                timestampsInSnapshots: true
+
+            })
+
+            window._initializedFirebase = true;
+        }
+    };
+
+    static db() { 
+
+        return firebase.firestore();
+    }
+
+    static hd() { 
+
+        return firebase.storage();
+    }
+
+    initAuth(){
+
+        return new Promise((s, f) => { 
+
+            let provider = new firebase.auth.GoogleAuthProvider()
+
+            firebase.auth().signInWithPopup(provider)
+            .then(result => { 
+
+                let token = result.credential.accessToken;
+                let user = result.user
+
+                s({
+                    user, 
+                    token
+                })
+
+            })
+            .catch(err=> { 
+                f(err)
+            })
+        })
+    }
+
+}
